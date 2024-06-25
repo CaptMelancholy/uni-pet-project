a309dee2c959bacd62dae7814bde4f3cd875b3c2
Artyom Danko <cflabravo@yahoo.com>
2024-06-25 12:15:03 +0300

    fix: testes refractoring

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,28 +1,22 @@
 import { describe, expect, test } from 'vitest';
 import Card from './Card';
-import { render, screen, cleanup } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { EStatuses, ICard } from './Card.types';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme } from '../../styles/theme';
+import { specifiedCardRender } from '../../tests/Render';
 
-describe('Card tests', () => {
-  afterEach(() => {
-    cleanup();
-  });
-
-  test('render with full parameters', () => {
-
+describe('Cards tests', () => {
+  test('render card with title, description, badges and deadline info with status, date and time', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
       badges: [
         {
           color: '#000000',
-          text: 'something',
+          text: 'Badge 1',
         },
       ],
-      title: '123',
-      desc: 'Hi',
+      title: 'Card Title',
+      desc: 'Card Description',
       deadlineInfo: {
         status: EStatuses.Completed,
         deadline_date: '22 jan 2024',
@@ -30,34 +24,30 @@ describe('Card tests', () => {
       },
     };
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
 
-    expect(screen.getByText('123')).toBeInTheDocument();
-    expect(screen.getByText('Hi')).toBeInTheDocument();
-    expect(screen.getAllByTestId('badge')).not.toBeNull();
-    
-    const badges = screen.getAllByTestId('badge');
-    expect(badges.length).toBeGreaterThanOrEqual(1);
-
-    expect(screen.getByTestId('date-time-container')).toBeInTheDocument();
+    expect(screen.getByText('Card Title')).toBeInTheDocument();
+    expect(screen.getByText('Card Description')).toBeInTheDocument();
+    expect(screen.getByText('Badge 1')).toBeInTheDocument();
     expect(screen.getByText('22 jan 2024')).toBeInTheDocument();
     expect(screen.getByText('00:00')).toBeInTheDocument();
   });
-  test('render without desc', () => {
+  test('render card with title, badges and deadline info with status, date and time, but without description', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
       badges: [
         {
           color: '#000000',
-          text: 'something',
+          text: 'Badge 1',
         },
       ],
-      title: '123',
+      title: 'Card Title',
       deadlineInfo: {
         status: EStatuses.Completed,
         deadline_date: '22 jan 2024',
@@ -65,151 +55,126 @@ describe('Card tests', () => {
       },
     };
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
+
     expect(screen.queryByTestId('desc')).toBeNull();
   });
-  test('render without time', () => {
+  test('render card with title, badges, but without time of deadline info', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
       badges: [
         {
           color: '#000000',
-          text: 'something',
+          text: 'Badge 1',
         },
       ],
-      title: '123',
-      desc: 'Hi',
+      title: 'Card Title',
       deadlineInfo: {
         status: EStatuses.Completed,
         deadline_date: '22 jan 2024',
       },
     };
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
+
     expect(screen.getByTestId('date-time-container')).toBeInTheDocument();
     expect(screen.getByText('22 jan 2024')).toBeInTheDocument();
-
-    const dateTimeContainer = screen.getByTestId('date-time-container');
-
-    expect(dateTimeContainer.childNodes.length).toBe(1);
   });
 
-  
-
-  test('render with empty badges', () => {
+  test('render card but with empty badges', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
       badges: [],
-      title: '123',
-      desc: 'Hi',
+      title: 'Card Title',
       deadlineInfo: {
         status: EStatuses.Completed,
         deadline_date: '22 jan 2024',
-        deadline_time: '00:00',
-      }
-    };
-
-      render(
-        <ThemeProvider theme={lightTheme}>
-          <Card index={0} card={cardTestingObject} />
-        </ThemeProvider>,
-      );
-
-      expect(screen.queryByTestId('badge-container')).toBeNull();
-    });
-
-
-  test('render with completed deadlines', () => {
-    const cardTestingObject: ICard = {
-      id: 0,
-      parent_id: 0,
-      badges: [
-        {
-          color: '#000000',
-          text: 'something',
-        },
-      ],
-      title: '123',
-      desc: 'Hi',
-      deadlineInfo: {
-        status: EStatuses.Completed,
-        deadline_date: '22 jan 2024',
-        deadline_time: '00:00',
       },
     };
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
+    );
+
+    expect(screen.queryByTestId('badge-container')).toBeNull();
+  });
+
+  test('render card with status completed deadlines', () => {
+    const cardTestingObject: ICard = {
+      id: 0,
+      parent_id: 0,
+      badges: [],
+      title: 'Card Title',
+      deadlineInfo: {
+        status: EStatuses.Completed,
+        deadline_date: '22 jan 2024',
+      },
+    };
+
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
     expect(screen.getByTestId('date-time-container')).toHaveStyle(
       'background-color: #93E1C0; color: #4BCE97',
     );
   });
 
-
-  test('render with deadlines', () => {
+  test('render card with status deadline', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
-      badges: [
-        {
-          color: '#000000',
-          text: 'something',
-        },
-      ],
-      title: '123',
-      desc: 'Hi',
+      badges: [],
+      title: 'Card Title',
       deadlineInfo: {
         status: EStatuses.Deadline,
         deadline_date: '22 jan 2024',
-        deadline_time: '00:00',
       },
     };
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
     expect(screen.getByTestId('date-time-container')).toHaveStyle(
       'background-color: #EFB4B4; color: #CC0707',
     );
   });
 
-
-  test('render with in progress', () => {
+  test('render card with status in progress', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
-      badges: [
-        {
-          color: '#000000',
-          text: 'something',
-        },
-      ],
-      title: '123',
-      desc: 'Hi',
+      badges: [],
+      title: 'Card Title',
       deadlineInfo: {
         status: EStatuses.InProgress,
         deadline_date: '22 jan 2024',
-        deadline_time: '00:00',
       },
     };
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
     expect(screen.getByTestId('date-time-container')).toHaveStyle(
       'background-color: #B1B3C5; color: #646570',
@@ -220,19 +185,14 @@ describe('Card tests', () => {
     const cardTestingObject: ICard = {
       id: 0,
       parent_id: 0,
-      badges: [
-        {
-          color: '#000000',
-          text: 'something',
-        },
-      ],
-      title: '123',
-      desc: 'Hi',
+      badges: [],
+      title: 'Card Title',
     };
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Card index={0} card={cardTestingObject} />
-      </ThemeProvider>,
+    specifiedCardRender(
+      <Card
+        index={0}
+        card={cardTestingObject}
+      />,
     );
     expect(screen.queryByTestId('date-time-container')).toBeNull();
   });
